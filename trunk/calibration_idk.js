e4a4a1f2ec29d03bfc19902566ea9662433c5426
gengkev@gmail.com <gengkev@gmail.com@be15f933-a9cd-2ede-e543-eb6f1d5445e7>
2012-04-13 00:23:44 +0000

    argh blah

--- trunk/calibration_idk.js
+++ trunk/calibration_idk.js
@@ -2,20 +2,59 @@ window.calibration_algorithms || (calibration_algorithms = []);
 window.update_sequences || (update_sequences = []);
 
 calibration_algorithms["idk"] = function(win) {
+	function fastabs(n){return ((n-1)>>31)?-n:n}
+
+	function init() { //for calculating camera delay
+		var canvas = document.createElement("canvas");
+		var ctx = canvas.getContext("2d");
+		canvas.width = screen.width; canvas.height = screen.height;
+		
+		canvas.style.border = "1px solid black";
+		cont.appendChild(document.createElement("br"));
+		cont.appendChild(canvas);
+		
+		// init done, calculate camera delay
+		
+		// first get a couple of diffs (CAPTURE_ATTEMPTS factorial)
+		const INTERVAL_MIN = 200, INTERVAL_MAX = 1000, CAPTURE_ATTEMPTS = 3;
+		
+		var pixelDatas = [];
+		for (i=0;i<CAPTURE_ATTEMPTS;i++) {
+			setTimeout(function(){
+				pixelDatas.push(ctx.getImageData(0,0,canvas.width,canvas.height));
+			},randInt(INTERVAL_MIN,INTERVAL_MAX));
+		}
+		setTimeout(function(){init2(pixelDatas)},INTERVAL_MAX * CAPTURE_ATTEMPTS);
+	}
+	function init2(pixelDatas) {
+		console.log(pixelDatas);
+	}
+	function calculateImageDiffLevel(olddata,newdata) {
+		if (olddata.length != newdata.length) {
+			throw new RangeError("lengths must be the same");
+		}
+		var total = 0, len = olddata.length;
+		for (i=0;i<len;i+=4) {
+			total += (olddata[i] + olddata[i+1] + olddata[i+2])
+			       - (newdata[i] + newdata[i+1] + newdata[i+2]);
+		}
+		console.log("did average diff thing: "+(total / len));
+		return total / len;
+	}
+	
+	/////////////////////////////////////////////////////////////////
+	var calibration_options = {
+		screen_width: screen.width / 2,
+		screen_height: screen.height / 2,
+		current_algorithm : "idk"
+	},
+	    i;
+	
 	win.$ = function(d){return win.document.getElementById(d)};
 	
 	var cont = win.$("container");
-	var canvas = document.createElement("canvas");
+	cont.innerHTML = 'Please drag this to the appropriate window and make it fullscreen before you '
+	               + '<button id="begin">begin</button>.';
+	win.$("begin").onclick = init;
 	
-	canvas.width = 640; canvas.height = 480;
-	
-	canvas.style.border = "1px solid black";
-	cont.appendChild(document.createElement("br"));
-	cont.appendChild(canvas);
-	var ctx = calibration_algorithms["idk"].ctx = canvas.getContext("2d");
-	
-	update_sequences.push(calibration_algorithms["idk"].update);
-
-}
-calibration_algorithms["idk"].update = function(){
 }
--- trunk/polyfills.js
+++ trunk/polyfills.js
@@ -1,23 +1,184 @@
+// The title is misleading, it's actually mostly convenience functions
+
 /* requestAnimationFrame polyfill
  * by Eric Moller
  * http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
  */
-(function(){var a=0,b=["ms","moz","webkit","o"];for(var c=0;c<b.length&&!window.requestAnimationFrame;++c)window.requestAnimationFrame=window[b[c]+"RequestAnimationFrame"],window.cancelRequestAnimationFrame=window[b[c]+"CancelRequestAnimationFrame"];window.requestAnimationFrame||(window.requestAnimationFrame=function(b,c){var d=(new Date).getTime(),e=Math.max(0,16-(d-a)),f=window.setTimeout(function(){b(d+e)},e);return a=d+e,f}),window.cancelAnimationFrame||(window.cancelAnimationFrame=function(a){clearTimeout(a)})})()
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelRequestAnimationFrame = window[vendors[x]+
+          'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}())
 
 /* fullscreen api something
  * by me
  * https://gist.github.com/2302950
  */
-function toggleFullScreen(a){!window.fullScreen&&!document.mozFullScreen&&!document.webkitIsFullScreen||document.fullscreenElement&&document.fullscreenElement===null||document.fullScreenElement&&document.fullScreenElement===null?(a=a instanceof Element?a:document.documentElement,["requestFullscreen","requestFullScreen","mozRequestFullScreen","webkitRequestFullscreen","webkitRequestFullScreen","webkitEnterFullscreen"].forEach(function(b){a[b]&&a[b]()})):["exitFullscreen","cancelFullScreen","mozCancelFullScreen","webkitCanvelFullscreen","webkitCancelFullScreen","webkitExitFullscreen"].forEach(function(a){document[a]&&document[a]()})}function fireEvent(a,b){if(document.createEventObject){var c=document.createEventObject();return a.fireEvent("on"+b,c)}var c=document.createEvent("Events");return c.initEvent(b,!0,!0),!a.dispatchEvent(c)}document.addEventListener("mozfullscreenchange",function(a){fireEvent(document,"fullscreenchange")},!1),document.addEventListener("webkitfullscreenchange",function(a){fireEvent(document,"fullscreenchange")},!1)
+function toggleFullScreen(el,override) {
+	if (override == true) {
+
+		el = (el instanceof Element) ? el : document.documentElement;
+
+		[ "requestFullscreen",
+		  "requestFullScreen",
+		  "mozRequestFullScreen",
+		  "webkitRequestFullscreen",
+		  "webkitRequestFullScreen",
+		  "webkitEnterFullscreen"].forEach(function(c) {
+			if (el[c]) {
+				el[c]();
+				return true;
+			}
+		});
+	} else if (override == false) {
+		[ "exitFullscreen",
+		  "cancelFullScreen",
+		  "mozCancelFullScreen",
+		  "webkitCanvelFullscreen",
+		  "webkitCancelFullScreen",
+		  "webkitExitFullscreen"].forEach(function(c) {
+			if (document[c]) {
+				document[c]();
+				return true;
+			}
+		});
+	} else {
+		if((!window.fullScreen && !document.mozFullScreen && !document.webkitIsFullScreen) ||
+		   (document.fullscreenElement && document.fullscreenElement === null) ||
+		   (document.fullScreenElement && document.fullScreenElement === null)) {
+			return toggleFullScreen(el,true);
+		} else {
+			return toggleFullScreen(el,false);
+		}
+	}
+	return false;
+}
+/* firing events from multiple places */
+function fireEvent(el, evtName) {
+	if (document.createEventObject) {
+		var evt = document.createEventObject();
+		return el.fireEvent("on" + evtName, evt);
+	} else {
+		var evt = document.createEvent("Events");
+		evt.initEvent(evtName, true, true);
+		return !el.dispatchEvent(evt);
+	}
+}
+
+// doesn't quite work
+document.addEventListener("mozfullscreenchange", function(e) {
+	fireEvent(document,"fullscreenchange");
+},false);
+document.addEventListener("webkitfullscreenchange", function(e) {
+	fireEvent(document,"fullscreenchange");
+},false);
 
 /* fake Uint8ClampedArray for browsers that support CanvasPixelArray
  * by me
  * https://gist.github.com/2295355
  */
-!window.Uint8ClampedArray&&Uint8Array&&function(){try{return document.createElement("canvas").getContext("2d").getImageData}catch(a){return!1}}()&&(window.Uint8ClampedArray=function(a,b,c){if(this instanceof Uint8ClampedArray){var d=0;if(a.length)d=a.length;else{if(a.constructor==ArrayBuffer)return new Uint8ClampedArray(new Uint8Array(a,b,c));d=Number(a),a=null}if(d!=Math.abs(d)<<0)throw new RangeError;d=Math.ceil(d/4)*4;var e=document.createElement("canvas");e.width=d/4,e.height=1;var f=e.getContext("2d").getImageData(0,0,d/4,1).data;if(a)for(var g=0;g<a.length;g++)f[g]=a[g];return Object.defineProperty?Object.defineProperty(f,"buffer",{get:function(){return(new Uint8Array(this)).buffer}}):Object.prototype.__defineGetter__&&f.__defineGetter__("buffer",function(){return(new Uint8Array(this)).buffer}),f}throw new TypeError("DOM object constructor cannot be called as a function.")})
+if (!window.Uint8ClampedArray && Uint8Array &&
+    (function(){
+    	try { return document.createElement("canvas").getContext("2d").getImageData; }
+    	catch(e) { return false; } 
+    }())) {
+window.Uint8ClampedArray = function(input,arg1,arg2) {
+	if (!(this instanceof Uint8ClampedArray)) {
+		throw new TypeError("DOM object constructor cannot be called as a function.");
+	}
+	var len = 0;
+	if (input.length) { //an array, yay
+		len = input.length;
+	} else if (input.constructor == ArrayBuffer) {
+		return new Uint8ClampedArray(new Uint8Array(input,arg1,arg2));
+	} else {
+		len = Number(input);
+		input = null;
+	}
+
+	if (len != Math.abs(len) << 0) { // also catches Number() parse fails
+		throw new RangeError();
+	}
+	len = Math.ceil(len / 4) * 4; // round up to the nearest mult of 4
+
+	var canvas = document.createElement("canvas");
+	canvas.width = len / 4; // the CanvasPixelArray will have dims 4 * width * height
+	canvas.height = 1;
+
+	var array = canvas.getContext("2d").getImageData(0,0,len/4,1).data;
+
+	if (input) {
+		for (var i=0;i<input.length;i++) {
+			array[i] = input[i];
+		}
+	}
+	if (Object.defineProperty) {
+		Object.defineProperty(array,"buffer",{
+			get: function() {
+				return new Uint8Array(this).buffer;
+			}
+		});
+	} else if (Object.prototype.__defineGetter__) {
+		array.__defineGetter__("buffer",function() {
+			return new Uint8Array(this).buffer;
+		});
+	}
+	return array;
+}
+}
 
 /* gUM shield!
- * by me, sort of, see
+ * half by me, sort of, see
  * https://gist.github.com/f9986533817575de62d8
  */
-function getUserMedia(a,b,c){navigator.getUserMedia||c(new Error("getUserMedia not supported"));try{navigator.getUserMedia(a,b,c)}catch(d){var e=[];for(var f in a)a[f]==1&&e.push(f);e=e.join(",");try{navigator.getUserMedia(e,b,c)}catch(d){c(new Error("getUserMedia not supported"))}}}window.URL||(window.URL=window.webkitURL||window.msURL||window.oURL),navigator.getUserMedia||(navigator.getUserMedia=navigator.webkitGetUserMedia||navigator.mozGetUserMedia||navigator.msGetUserMedia)
+//normalize window.URL
+window.URL || (window.URL = window.webkitURL || window.msURL || window.oURL);
+
+//normalize navigator.getUserMedia
+navigator.getUserMedia || (navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+
+function getUserMedia(opts, success, fail) {
+	if (!navigator.getUserMedia) fail(new Error("getUserMedia not supported"));
+
+	try {
+		navigator.getUserMedia(opts,success,fail);
+	} catch(e) {
+		var stringopts = [];
+		for (var i in opts) {
+			if (opts[i] == true) {
+				stringopts.push(i);
+			}
+		}
+		stringopts = stringopts.join(",");
+		try {
+			navigator.getUserMedia(stringopts,success,fail);
+		} catch(e) {
+			fail(new Error("getUserMedia not supported"));
+		}
+	}
+}
+
+/* randInt
+ * by me
+ * https://gist.github.com/2371754
+ */
+function randInt(a,b){return a+Math.floor(Math.random()*(++b-a))}
