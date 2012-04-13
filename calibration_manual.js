11a8e4fd1c7e0b0bca1e42ac7907af73cf65deea
gengkev@gmail.com <gengkev@gmail.com@be15f933-a9cd-2ede-e543-eb6f1d5445e7>
2012-04-13 00:23:44 +0000

    argh blah

--- calibration_manual.js
+++ calibration_manual.js
@@ -2,6 +2,24 @@ window.calibration_algorithms || (calibration_algorithms = []);
 window.update_sequences || (update_sequences = []);
 
 calibration_algorithms["manual"] = function(win) {
+	// helper functions
+	
+	function update() {
+		// uhh we have ctx in this scope right?
+		// var ctx = calibration_algorithms["manual"].ctx;
+		
+		// be cheap and copy off the other one to save processing
+		ctx.drawImage(output,0,0,canvas.width,canvas.height);
+	}
+	
+	/////////////////////////////////////////////////////////////
+	calibration_options = {
+		screen_width: screen.width,
+		screen_height: screen.height,
+		current_algorithm : "manual"
+	}
+	
+	
 	win.$ = function(d){return win.document.getElementById(d)};
 	
 	var cont = win.$("container");
@@ -14,15 +32,18 @@ calibration_algorithms["manual"] = function(win) {
 	canvas.style.border = "1px solid black";
 	cont.appendChild(document.createElement("br"));
 	cont.appendChild(canvas);
-	var ctx = calibration_algorithms["manual"].ctx = canvas.getContext("2d");
 	
-	// window.requestAnimationFrame(calibration_algorithms["manual"].update);
-	update_sequences.push(calibration_algorithms["manual"].update);
+	var ctx = canvas.getContext("2d");
+	ctx.strokeStyle = "#FF0000";
+	// ctx.lineCap = "";
+	// ctx.lineJoin = "";
+	
+	update_sequences.push(update);
 	
 	var locs = ["top left","top right","bottom right","bottom left"];
 	loc.innerHTML = locs[0];
 	var pos = 0;
-	ctx.strokeStyle = "#FF0000";
+	
 	canvas.onclick = function(e) {
 		var x,y;
 		if (e.pageX && e.pageY) { 
@@ -51,11 +72,14 @@ calibration_algorithms["manual"] = function(win) {
 			// cancel update thing
 			// or not, who cares?
 			// yes i care
-			// update_sequences.splice(update_sequences.indexOf(calibration_algorithms["manual"].update),1);
+			// update_sequences.splice(update_sequences.indexOf(update),1);
 			
 			var b = document.createElement("b");
 			b.appendChild(document.createTextNode("You can\/should close the window now."));
 			cont.appendChild(b);
+			
+			canvas.onclick = null;
+			//win.close();
 			
 			calculateQuad();
 		}
@@ -63,19 +87,4 @@ calibration_algorithms["manual"] = function(win) {
 		// ctx.strokeRect(x-10, y-10, 20, 20);
 		
 	}
-}
-calibration_algorithms["manual"].update = function(){
-	var ctx = calibration_algorithms["manual"].ctx;
-	
-	/*
-	ctx.drawImage(video,0,0);
-	for (var i in quad) {
-		if (quad[i] != null) {
-			ctx.strokeRect(quad[i][0] - 10, quad[i][1] - 10, 20, 20);
-		}
-	}
-	*/
-	
-	// be cheap and copy off the other one to save processing
-	ctx.drawImage(output,0,0,640,480);
 }
