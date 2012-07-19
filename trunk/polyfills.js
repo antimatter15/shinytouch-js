b334a7227505c9b2945e7e823bda5a32fa756fe4
gengkev@gmail.com <gengkev@gmail.com@be15f933-a9cd-2ede-e543-eb6f1d5445e7>
2012-07-19 03:02:54 +0000

    Refactored some code, and now the auto-calibration algorithm is done! Well, it's not perfect because of some math stuff with the screen coordinates I need to fix up, but close enough.

--- trunk/polyfills.js
+++ trunk/polyfills.js
@@ -178,3 +178,28 @@ function getUserMedia(opts, success, fail) {
  * https://gist.github.com/2371754
  */
 function randInt(a,b){return a+Math.floor(Math.random()*(++b-a))}
+
+
+/* RGB to HSL conversion
+ * http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+ */
+function rgbToHsl(r, g, b){
+    r /= 255, g /= 255, b /= 255;
+    var max = Math.max(r, g, b), min = Math.min(r, g, b);
+    var h, s, l = (max + min) / 2;
+
+    if(max == min){
+        h = s = 0; // achromatic
+    }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
+}
